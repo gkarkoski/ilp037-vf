0a28beaf92366047fc63fcc5507abcfa6daca3aa
gkarkoski <168877235+gkarkoski@users.noreply.github.com>
2024-05-22 21:54:03 -0300

    Update App.java

--- CreationalPattern/src/ilp037/creational/abstractfactory/App.java
+++ CreationalPattern/src/ilp037/creational/abstractfactory/App.java
@@ -1,7 +1,7 @@
 package ilp037.creational.abstractfactory;
 
 public class App {
-
+//edição
 	public static void main(String[] args) {
 		String type ="Orc";
 		KingdomFactory Factory = KingdomFactory.getinstance(type);
